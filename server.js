91fc2ea6ee156a700ae97f6cba5e10f6a800cafa
Kirill <kirillsavorin@gmail.com>
2024-12-15 15:02:54 +0100

    server settings update

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+
 const movies = [
     { 
       id: 1, 
